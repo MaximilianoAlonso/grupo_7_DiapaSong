86543b80e185dda84022ffc03c23fdcc1f308b0d
Maximiliano <elgrungemaxi1986@gmail.com>
2023-06-15 14:05:02 -0300

    asd

--- api/user.js
+++
@@ -1,5 +0,0 @@
-module.exports = function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({ name: 'John Doe' });
-};
--- index.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-const { v4 } = require('uuid');
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-module.exports = app;
